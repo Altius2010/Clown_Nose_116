1deb11c33674c228a46201bc2773961c64e4ef37
Altius2010 <72147694+Altius2010@users.noreply.github.com>
2021-01-26 16:38:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var nose_x = 0;
+var nose_y = 0;
+
+function preload() {
+    clownnose = loadImage("https://i.postimg.cc/rpxgkSB2/clownnose.png");
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Pose Net is loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose X = " + results[0].pose.nose.x);
+        console.log("nose Y = " + results[0].pose.nose.y);
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clownnose, nose_x - 15, nose_y - 15, 30, 30);
+}
+
+function takeSnapshot() {
+    save("my_filter.png");
+}
